453ed13b0254ff0ad5ecfb3ab41d0df9b91a768f
Kim Moberg <kimskogfelter@outlook.com>
2024-04-16 07:31:10 +0000

    added props to the component in the app

--- ci-react-app-walkthru/src/App.js
+++ ci-react-app-walkthru/src/App.js
@@ -7,7 +7,7 @@ function App() {
   return (
     <div className="App">
       <FunctionalGreeting />
-      <FunctionalGreetingWithProps />
+      <FunctionalGreetingWithProps greeting="nice to meet you!" name="Kim" age="32"/>
     </div>
   );
 }
